ec42ab21f5700e83f8f9c61a941764edeb2d8f0a
Matthew Kukla <mkukla2000@outlook.com>
2026-01-24 17:55:16 -0600

    chore(04-05): add barrel export for journal components
    - Exports JournalGrid, StageCell, ContactNameCell, EventTimelineDrawer
    - Simplifies imports in JournalDetail page

---
+++ frontend/src/pages/journals/components/index.ts
@@ -0,0 +1,4 @@
+export { JournalGrid } from "./JournalGrid"
+export { StageCell } from "./StageCell"
+export { ContactNameCell } from "./ContactNameCell"
+export { EventTimelineDrawer } from "./EventTimelineDrawer"
