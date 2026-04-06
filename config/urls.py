1571943fd6a001b5f885c331988ac52ddcbfeb9b
Matthew Kukla <mkukla2000@outlook.com>
2026-04-06 15:55:08 -0500

    changed backstage back to admin

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('backstage/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/v1/', include('config.api_urls')),
 ]
 
